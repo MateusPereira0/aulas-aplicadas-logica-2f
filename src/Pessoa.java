7ddd5f5b3dd1c89bb55c037def27e0d4a7a6d557
MateusPereira0 <maat015z@gmail.com>
2023-05-25 08:05:56 -0300

    novo projeto

---
+++ src/Pessoa.java
@@ -0,0 +1,16 @@
+public class Pessoa {
+    // Atributos da classe
+
+    String nome;
+    String sobrenome;
+
+    // Método da classe
+    public void falar() {
+        System.out.println("falei ");
+  }
+
+  public String falar(String volume) {
+      return "falei " + volume;
+    }
+
+  }
